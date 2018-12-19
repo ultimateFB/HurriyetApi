04ca7c816772b46429b0fd62652765d4d0d90ab8
Mehmet VARIŞLI <mehmetvarisli42@gmail.com>
2018-12-19 20:58:38 +0300

    Create PageDetail.php

---
+++ PageDetail.php
@@ -0,0 +1,30 @@
+<?php
+$page_id="5581488665345c152c269bc0";
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.hurriyet.com.tr/v1/pages/".$page_id,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "accept: application/json",
+    "apikey: YOUR API KEY HERE"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+ $array=json_decode($response, True);
+ print_r($array);
+
+}
