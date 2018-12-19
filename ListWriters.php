c6775c2d0daa3d6a9911ec7718c0cbc1482a11f7
Mehmet VARIŞLI <mehmetvarisli42@gmail.com>
2018-12-19 20:16:57 +0300

    Create ListWriters.php

---
+++ ListWriters.php
@@ -0,0 +1,29 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.hurriyet.com.tr/v1/writers",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "accept: application/json",
+    "apikey: API-KEY-HERE"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  $array=json_decode($response, True);
+  print_r($array);
+}
