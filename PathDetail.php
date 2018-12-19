784573c0cc767a85f86703e5c56cacdd230d333b
Mehmet VARIŞLI <mehmetvarisli42@gmail.com>
2018-12-19 21:04:50 +0300

    Create PathDetail.php

---
+++ PathDetail.php
@@ -0,0 +1,31 @@
+
+<?php
+$path_id="563cddcc67b0a934e44ee2d7";
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.hurriyet.com.tr/v1/paths/".$path_id,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "accept: application/json",
+    "apikey: API KEY HERE"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+ $array=json_decode($response, True);
+ print_r($array);
+
+}
